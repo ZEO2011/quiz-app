c798880f0ae8df79c8643479838f84414a0a70d2
Ziad hatem <hatemziad50@gmail.com>
2023-07-24 16:35:54 +0300

    quiz app completed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./dist/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				main: "#0075ff",
+			},
+		},
+	},
+	plugins: [],
+};
